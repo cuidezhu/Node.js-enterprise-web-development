eb33619d1b789c650a78045b69a054d7f807fbc9
cuidezhu <chinacuidezhu@gmail.com>
2019-05-02 15:39:47 +0800

    exports = module.exports

---
+++ demos/08_exps.js
@@ -0,0 +1,15 @@
+// const exports = module.exports;
+
+// (
+//   function(exports, require, module, __filenamde, __dirname) {
+
+//   }
+// );
+
+// exports.test = 100;
+
+module.exports = {
+  a: 1,
+  b: 2,
+  test: 100
+};
